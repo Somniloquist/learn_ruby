129b52ba377f1617b8b556f3b4fdcad992228bf0
Robin Unger <robin.e.unger@gmail.com>
2019-01-30 16:46:21 -0600

    Completed exercise 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,53 @@
 #write your code here
+def echo phrase
+    phrase
+end
+
+def shout phrase
+    phrase.upcase
+end
+
+def repeat phrase, n = 2
+    result = []
+    n.times do
+        result << phrase
+    end
+    result.join(" ")
+end
+
+def start_of_word word, n
+    word[0, n]
+end
+
+def first_word word
+    word[0, word.index(" ")]
+end
+
+def titleize word
+    word.gsub(/\b\w/) do |match|
+        match.upcase
+    end
+end
+
+def little_word? word
+    little_words = ["and", "the", "over"]
+    little_words.each do |little_word|
+        if word == little_word
+            return true
+        end
+    end
+
+    return false
+end
+
+def titleize title
+    title = title.split
+    result = []
+    title.each do |word|
+        little_word?(word) ? result << word : result << word.capitalize
+    end
+    
+    #capitalize the first letter
+    result[0][0] = result[0][0].capitalize
+    result = result.join(" ")
+end

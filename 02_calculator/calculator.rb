bac8dc6ab08baf8e148bf015fde2a15551065119
Robin Unger <robin.e.unger@gmail.com>
2019-01-30 15:06:56 -0600

    completed exercise 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+def add a, b
+    a + b
+end
+
+def subtract a, b
+    a - b
+end
+
+def sum numbers
+    numbers.reduce(0) { |sum, n| sum += n }
+end
+
+def multiply numbers
+    numbers.reduce(1) { |result, n| result *= n }
+end
+
+def power a, b
+    a**b
+end
+
+def factorial a
+    a <=1 ? 1 : (2..a).reduce(:*)
+end

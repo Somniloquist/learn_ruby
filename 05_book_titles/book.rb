cd53230d31c8582e952335d84ae3de39970d5b6b
Robin Unger <robin.e.unger@gmail.com>
2019-02-02 15:46:32 -0600

    Completed exercise 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
-# write your code here
+    attr_accessor :title
+
+    def title
+        exceptions = ["and", "the", "in", "of", "a", "an"]
+        book_title = []
+        title = @title.split()
+
+        title.each do |word|
+            exceptions.include?(word) ? book_title << word : book_title << word.capitalize
+        end
+
+        book_title[0] = book_title.first.capitalize
+
+        book_title.join(" ")
+    end
 end
